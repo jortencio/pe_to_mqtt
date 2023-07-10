11481e47cbb5d895e2008817fcb52f3ec633f2e2
Jason Ortencio <jason.ortencio@puppet.com>
2023-07-10 14:03:16 +1000

    Fix Linting

--- lib/puppet/reports/report_mqtt.rb
+++ lib/puppet/reports/report_mqtt.rb
@@ -16,13 +16,11 @@ Puppet::Reports.register_report(:report_mqtt) do
       Puppet.info 'report_mqtt is disabled, no report data published to mqtt'
     else
       begin
-
         report = if mqtt_config['report'].key?('select_fields')
-          self.to_data_hash.select { |k, _v| mqtt_config['report']['select_fields'].exclude?(k) }
-        else
-          self.to_data_hash
-        end
-        
+                   self.to_data_hash.select { |k, _v| mqtt_config['report']['select_fields'].exclude?(k) }
+                 else
+                   self.to_data_hash
+                 end
 
         Puppet.info 'Connecting to MQTT Broker: ' + mqtt_config['mqtt']['hostname']
         mqtt_conn = MQTT::Client.connect(host: mqtt_config['mqtt']['hostname'], port: mqtt_config['mqtt']['port'])
