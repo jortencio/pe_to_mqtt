ce6f96e541989c4361a46f87658b6ef0349e4b64
Jason Ortencio <jason.ortencio@puppet.com>
2023-07-10 13:53:26 +1000

    Add ability to select fields in report

--- lib/puppet/reports/report_mqtt.rb
+++ lib/puppet/reports/report_mqtt.rb
@@ -16,12 +16,20 @@ Puppet::Reports.register_report(:report_mqtt) do
       Puppet.info 'report_mqtt is disabled, no report data published to mqtt'
     else
       begin
+
+        report = if mqtt_config['report'].key?('select_fields')
+          self.to_data_hash.select { |k, _v| mqtt_config['report']['select_fields'].exclude?(k) }
+        else
+          self.to_data_hash
+        end
+        
+
         Puppet.info 'Connecting to MQTT Broker: ' + mqtt_config['mqtt']['hostname']
         mqtt_conn = MQTT::Client.connect(host: mqtt_config['mqtt']['hostname'], port: mqtt_config['mqtt']['port'])
 
         if mqtt_config['report']['publish_status'] == 'all' || self.status == mqtt_config['report']['publish_status']
           Puppet.info 'Publish report to MQTT Broker. Topic: ' + mqtt_config['report']['topic']
-          mqtt_conn.publish(mqtt_config['report']['topic'], self.to_data_hash)
+          mqtt_conn.publish(mqtt_config['report']['topic'], report)
         end
       rescue => e
         Puppet.err 'Error in report_mqtt report processor.  Message: ' + e.message
