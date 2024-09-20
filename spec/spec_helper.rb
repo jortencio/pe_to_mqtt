5c3f88bc2c702cab1302de81f01f54a3bbb06de5
Jason Ortencio <jason.ortencio@puppet.com>
2024-09-20 13:47:56 +1000

    Remove duplicated confdir setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,7 +43,6 @@ RSpec.configure do |c|
   c.before :each do
     # set to strictest setting for testing
     # by default Puppet runs at warning level
-    Puppet.settings[:confdir] = '/etc/puppetlabs/puppet'
     Puppet.settings[:strict] = :warning
     Puppet.settings[:strict_variables] = true
   end
